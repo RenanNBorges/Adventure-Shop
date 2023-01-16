f5058ab0d6b23a79a9b9c0ac9c5e6ba1eca79398
Renan Borges <renan.n.borges.zohoemail.com>
2023-01-16 00:06:21 -0300

    model

---
+++ model/Produto.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const produtoSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  description: {
+    type: String,
+    require: true,
+  },
+  linkImage: {
+    type: String,
+    require: true,
+  },
+  price: {
+    type: Number,
+    require: true,
+  },
+});
+
+module.exports = mongoose.model("Produto", produtoSchema);
