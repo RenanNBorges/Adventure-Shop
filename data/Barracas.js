c08c6bb0f723a9868f454ecc253cffcc9c475f30
Renan Borges <renan.n.borges.zohoemail.com>
2023-01-16 08:43:40 -0300

    css

---
+++ data/Barracas.js
@@ -0,0 +1,179 @@
+const barracas = [
+    {
+    name: {
+        type:"Barraca de Camping Nautika Panda 2 pessoas Coluna d'água de 600mm",
+        require: true,
+      },
+      description: {
+        type: "Indicada para clima seco, com a barraca de camping Panda NTK da Nautika apreciar a natureza com seus amigos e família é muito mais divertido.  A barraca em formato Iglu tem coluna d'água de 600 mm, tela mosquiteiro de poliéster No-see-um e é ideal para clima seco. É prática, confortável, fácil de carregar e de montar.  Possui paredes em 100% poliéster com poliuretano, estrutura composta por varetas de fibra de vidro interligadas por elástico interno e fabricadas com material 100% virgem com sistema NANO-FLEX. Seu piso é de polietileno de alta resistência e antifungos.  Comporta até 2 pessoas em saco de dormir ou colchão.  Acompanha conjunto de estacas e cordas para fixação, conjunto de varetas e sacola para transporte. Na parte interna, possui bolso para organização de pequenos objetos.  O produto pesa apenas 1,5 kg, tem medidas de 1,05 m (A) x 1,45 m (L) x 2,05 m (P) e está disponível na cor azul.",
+        require: true,
+      },
+      linkImage: {
+        type: "https://nautika.vteximg.com.br/arquivos/ids/2654850-400-400/barraca-panda-2_000_155100_7896558421526_01.jpg",
+        require: true,
+      },
+      price: {
+        type: 210.00,
+        require: true,
+      },
+      name: {
+        type:"Barraca de Camping Nautika Panda 2 pessoas Coluna d'água de 600mm",
+        require: true,
+      },
+      description: {
+        type: "Indicada para clima seco, com a barraca de camping Panda NTK da Nautika apreciar a natureza com seus amigos e família é muito mais divertido.  A barraca em formato Iglu tem coluna d'água de 600 mm, tela mosquiteiro de poliéster No-see-um e é ideal para clima seco. É prática, confortável, fácil de carregar e de montar.  Possui paredes em 100% poliéster com poliuretano, estrutura composta por varetas de fibra de vidro interligadas por elástico interno e fabricadas com material 100% virgem com sistema NANO-FLEX. Seu piso é de polietileno de alta resistência e antifungos.  Comporta até 2 pessoas em saco de dormir ou colchão.  Acompanha conjunto de estacas e cordas para fixação, conjunto de varetas e sacola para transporte. Na parte interna, possui bolso para organização de pequenos objetos.  O produto pesa apenas 1,5 kg, tem medidas de 1,05 m (A) x 1,45 m (L) x 2,05 m (P) e está disponível na cor azul.",
+        require: true,
+      },
+      linkImage: {
+        type: "https://nautika.vteximg.com.br/arquivos/ids/2654850-400-400/barraca-panda-2_000_155100_7896558421526_01.jpg",
+        require: true,
+      },
+      price: {
+        type: 210.00,
+        require: true,
+      },
+      name: {
+        type:"Barraca de Camping Nautika Panda 2 pessoas Coluna d'água de 600mm",
+        require: true,
+      },
+      description: {
+        type: "Indicada para clima seco, com a barraca de camping Panda NTK da Nautika apreciar a natureza com seus amigos e família é muito mais divertido.  A barraca em formato Iglu tem coluna d'água de 600 mm, tela mosquiteiro de poliéster No-see-um e é ideal para clima seco. É prática, confortável, fácil de carregar e de montar.  Possui paredes em 100% poliéster com poliuretano, estrutura composta por varetas de fibra de vidro interligadas por elástico interno e fabricadas com material 100% virgem com sistema NANO-FLEX. Seu piso é de polietileno de alta resistência e antifungos.  Comporta até 2 pessoas em saco de dormir ou colchão.  Acompanha conjunto de estacas e cordas para fixação, conjunto de varetas e sacola para transporte. Na parte interna, possui bolso para organização de pequenos objetos.  O produto pesa apenas 1,5 kg, tem medidas de 1,05 m (A) x 1,45 m (L) x 2,05 m (P) e está disponível na cor azul.",
+        require: true,
+      },
+      linkImage: {
+        type: "https://nautika.vteximg.com.br/arquivos/ids/2654850-400-400/barraca-panda-2_000_155100_7896558421526_01.jpg",
+        require: true,
+      },
+      price: {
+        type: 210.00,
+        require: true,
+      },
+      name: {
+        type:"Barraca de Camping Nautika Panda 2 pessoas Coluna d'água de 600mm",
+        require: true,
+      },
+      description: {
+        type: "Indicada para clima seco, com a barraca de camping Panda NTK da Nautika apreciar a natureza com seus amigos e família é muito mais divertido.  A barraca em formato Iglu tem coluna d'água de 600 mm, tela mosquiteiro de poliéster No-see-um e é ideal para clima seco. É prática, confortável, fácil de carregar e de montar.  Possui paredes em 100% poliéster com poliuretano, estrutura composta por varetas de fibra de vidro interligadas por elástico interno e fabricadas com material 100% virgem com sistema NANO-FLEX. Seu piso é de polietileno de alta resistência e antifungos.  Comporta até 2 pessoas em saco de dormir ou colchão.  Acompanha conjunto de estacas e cordas para fixação, conjunto de varetas e sacola para transporte. Na parte interna, possui bolso para organização de pequenos objetos.  O produto pesa apenas 1,5 kg, tem medidas de 1,05 m (A) x 1,45 m (L) x 2,05 m (P) e está disponível na cor azul.",
+        require: true,
+      },
+      linkImage: {
+        type: "https://nautika.vteximg.com.br/arquivos/ids/2654850-400-400/barraca-panda-2_000_155100_7896558421526_01.jpg",
+        require: true,
+      },
+      price: {
+        type: 210.00,
+        require: true,
+      },
+      name: {
+        type:"Barraca de Camping Nautika Panda 2 pessoas Coluna d'água de 600mm",
+        require: true,
+      },
+      description: {
+        type: "Indicada para clima seco, com a barraca de camping Panda NTK da Nautika apreciar a natureza com seus amigos e família é muito mais divertido.  A barraca em formato Iglu tem coluna d'água de 600 mm, tela mosquiteiro de poliéster No-see-um e é ideal para clima seco. É prática, confortável, fácil de carregar e de montar.  Possui paredes em 100% poliéster com poliuretano, estrutura composta por varetas de fibra de vidro interligadas por elástico interno e fabricadas com material 100% virgem com sistema NANO-FLEX. Seu piso é de polietileno de alta resistência e antifungos.  Comporta até 2 pessoas em saco de dormir ou colchão.  Acompanha conjunto de estacas e cordas para fixação, conjunto de varetas e sacola para transporte. Na parte interna, possui bolso para organização de pequenos objetos.  O produto pesa apenas 1,5 kg, tem medidas de 1,05 m (A) x 1,45 m (L) x 2,05 m (P) e está disponível na cor azul.",
+        require: true,
+      },
+      linkImage: {
+        type: "https://nautika.vteximg.com.br/arquivos/ids/2654850-400-400/barraca-panda-2_000_155100_7896558421526_01.jpg",
+        require: true,
+      },
+      price: {
+        type: 210.00,
+        require: true,
+      },
+      name: {
+        type:"Barraca de Camping Nautika Panda 2 pessoas Coluna d'água de 600mm",
+        require: true,
+      },
+      description: {
+        type: "Indicada para clima seco, com a barraca de camping Panda NTK da Nautika apreciar a natureza com seus amigos e família é muito mais divertido.  A barraca em formato Iglu tem coluna d'água de 600 mm, tela mosquiteiro de poliéster No-see-um e é ideal para clima seco. É prática, confortável, fácil de carregar e de montar.  Possui paredes em 100% poliéster com poliuretano, estrutura composta por varetas de fibra de vidro interligadas por elástico interno e fabricadas com material 100% virgem com sistema NANO-FLEX. Seu piso é de polietileno de alta resistência e antifungos.  Comporta até 2 pessoas em saco de dormir ou colchão.  Acompanha conjunto de estacas e cordas para fixação, conjunto de varetas e sacola para transporte. Na parte interna, possui bolso para organização de pequenos objetos.  O produto pesa apenas 1,5 kg, tem medidas de 1,05 m (A) x 1,45 m (L) x 2,05 m (P) e está disponível na cor azul.",
+        require: true,
+      },
+      linkImage: {
+        type: "https://nautika.vteximg.com.br/arquivos/ids/2654850-400-400/barraca-panda-2_000_155100_7896558421526_01.jpg",
+        require: true,
+      },
+      price: {
+        type: 210.00,
+        require: true,
+      },
+      name: {
+        type:"Barraca de Camping Nautika Panda 2 pessoas Coluna d'água de 600mm",
+        require: true,
+      },
+      description: {
+        type: "Indicada para clima seco, com a barraca de camping Panda NTK da Nautika apreciar a natureza com seus amigos e família é muito mais divertido.  A barraca em formato Iglu tem coluna d'água de 600 mm, tela mosquiteiro de poliéster No-see-um e é ideal para clima seco. É prática, confortável, fácil de carregar e de montar.  Possui paredes em 100% poliéster com poliuretano, estrutura composta por varetas de fibra de vidro interligadas por elástico interno e fabricadas com material 100% virgem com sistema NANO-FLEX. Seu piso é de polietileno de alta resistência e antifungos.  Comporta até 2 pessoas em saco de dormir ou colchão.  Acompanha conjunto de estacas e cordas para fixação, conjunto de varetas e sacola para transporte. Na parte interna, possui bolso para organização de pequenos objetos.  O produto pesa apenas 1,5 kg, tem medidas de 1,05 m (A) x 1,45 m (L) x 2,05 m (P) e está disponível na cor azul.",
+        require: true,
+      },
+      linkImage: {
+        type: "https://nautika.vteximg.com.br/arquivos/ids/2654850-400-400/barraca-panda-2_000_155100_7896558421526_01.jpg",
+        require: true,
+      },
+      price: {
+        type: 210.00,
+        require: true,
+      },
+      name: {
+        type:"Barraca de Camping Nautika Panda 2 pessoas Coluna d'água de 600mm",
+        require: true,
+      },
+      description: {
+        type: "Indicada para clima seco, com a barraca de camping Panda NTK da Nautika apreciar a natureza com seus amigos e família é muito mais divertido.  A barraca em formato Iglu tem coluna d'água de 600 mm, tela mosquiteiro de poliéster No-see-um e é ideal para clima seco. É prática, confortável, fácil de carregar e de montar.  Possui paredes em 100% poliéster com poliuretano, estrutura composta por varetas de fibra de vidro interligadas por elástico interno e fabricadas com material 100% virgem com sistema NANO-FLEX. Seu piso é de polietileno de alta resistência e antifungos.  Comporta até 2 pessoas em saco de dormir ou colchão.  Acompanha conjunto de estacas e cordas para fixação, conjunto de varetas e sacola para transporte. Na parte interna, possui bolso para organização de pequenos objetos.  O produto pesa apenas 1,5 kg, tem medidas de 1,05 m (A) x 1,45 m (L) x 2,05 m (P) e está disponível na cor azul.",
+        require: true,
+      },
+      linkImage: {
+        type: "https://nautika.vteximg.com.br/arquivos/ids/2654850-400-400/barraca-panda-2_000_155100_7896558421526_01.jpg",
+        require: true,
+      },
+      price: {
+        type: 210.00,
+        require: true,
+      },
+      name: {
+        type:"Barraca de Camping Nautika Panda 2 pessoas Coluna d'água de 600mm",
+        require: true,
+      },
+      description: {
+        type: "Indicada para clima seco, com a barraca de camping Panda NTK da Nautika apreciar a natureza com seus amigos e família é muito mais divertido.  A barraca em formato Iglu tem coluna d'água de 600 mm, tela mosquiteiro de poliéster No-see-um e é ideal para clima seco. É prática, confortável, fácil de carregar e de montar.  Possui paredes em 100% poliéster com poliuretano, estrutura composta por varetas de fibra de vidro interligadas por elástico interno e fabricadas com material 100% virgem com sistema NANO-FLEX. Seu piso é de polietileno de alta resistência e antifungos.  Comporta até 2 pessoas em saco de dormir ou colchão.  Acompanha conjunto de estacas e cordas para fixação, conjunto de varetas e sacola para transporte. Na parte interna, possui bolso para organização de pequenos objetos.  O produto pesa apenas 1,5 kg, tem medidas de 1,05 m (A) x 1,45 m (L) x 2,05 m (P) e está disponível na cor azul.",
+        require: true,
+      },
+      linkImage: {
+        type: "https://nautika.vteximg.com.br/arquivos/ids/2654850-400-400/barraca-panda-2_000_155100_7896558421526_01.jpg",
+        require: true,
+      },
+      price: {
+        type: 210.00,
+        require: true,
+      },
+      name: {
+        type:"Barraca de Camping Nautika Panda 2 pessoas Coluna d'água de 600mm",
+        require: true,
+      },
+      description: {
+        type: "Indicada para clima seco, com a barraca de camping Panda NTK da Nautika apreciar a natureza com seus amigos e família é muito mais divertido.  A barraca em formato Iglu tem coluna d'água de 600 mm, tela mosquiteiro de poliéster No-see-um e é ideal para clima seco. É prática, confortável, fácil de carregar e de montar.  Possui paredes em 100% poliéster com poliuretano, estrutura composta por varetas de fibra de vidro interligadas por elástico interno e fabricadas com material 100% virgem com sistema NANO-FLEX. Seu piso é de polietileno de alta resistência e antifungos.  Comporta até 2 pessoas em saco de dormir ou colchão.  Acompanha conjunto de estacas e cordas para fixação, conjunto de varetas e sacola para transporte. Na parte interna, possui bolso para organização de pequenos objetos.  O produto pesa apenas 1,5 kg, tem medidas de 1,05 m (A) x 1,45 m (L) x 2,05 m (P) e está disponível na cor azul.",
+        require: true,
+      },
+      linkImage: {
+        type: "https://nautika.vteximg.com.br/arquivos/ids/2654850-400-400/barraca-panda-2_000_155100_7896558421526_01.jpg",
+        require: true,
+      },
+      price: {
+        type: 210.00,
+        require: true,
+      },
+      name: {
+        type:"Barraca de Camping Nautika Panda 2 pessoas Coluna d'água de 600mm",
+        require: true,
+      },
+      description: {
+        type: "Indicada para clima seco, com a barraca de camping Panda NTK da Nautika apreciar a natureza com seus amigos e família é muito mais divertido.  A barraca em formato Iglu tem coluna d'água de 600 mm, tela mosquiteiro de poliéster No-see-um e é ideal para clima seco. É prática, confortável, fácil de carregar e de montar.  Possui paredes em 100% poliéster com poliuretano, estrutura composta por varetas de fibra de vidro interligadas por elástico interno e fabricadas com material 100% virgem com sistema NANO-FLEX. Seu piso é de polietileno de alta resistência e antifungos.  Comporta até 2 pessoas em saco de dormir ou colchão.  Acompanha conjunto de estacas e cordas para fixação, conjunto de varetas e sacola para transporte. Na parte interna, possui bolso para organização de pequenos objetos.  O produto pesa apenas 1,5 kg, tem medidas de 1,05 m (A) x 1,45 m (L) x 2,05 m (P) e está disponível na cor azul.",
+        require: true,
+      },
+      linkImage: {
+        type: "https://nautika.vteximg.com.br/arquivos/ids/2654850-400-400/barraca-panda-2_000_155100_7896558421526_01.jpg",
+        require: true,
+      },
+      price: {
+        type: 210.00,
+        require: true,
+      },
+    }];
